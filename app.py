e675a08ae9e10cba2a5f88c67dd2c09832cc7605
toxicologist <toxiccologist@gmail.com>
2024-01-25 16:22:30 -0300

    new strats

--- app.py
+++ app.py
@@ -4,38 +4,46 @@ import requests
 
 app = Flask(__name__)
 
+USE_REAL_API = True
 
 @app.route('/get-strategy', methods=['POST'])
 def get_strategy():
     data = request.json
 
-    url = 'https://api.quantumnumbers.anu.edu.au'
-    key = data.get('apiKey')
-    app.logger.info(f"Key:{key}")
-    # current strategy is this: generate one int for each strategy
-    # maximum int = strategy to choose
+    if USE_REAL_API:
+        url = 'https://api.quantumnumbers.anu.edu.au'
+        key = data.get('apiKey')
+        app.logger.info(f"Key:{key}")
+        # current strategy is this: generate one int for each strategy
+        # maximum int = strategy to choose
 
-    r = requests.get(url, headers={'x-api-key': key}, params={
-        'length': len(strategies),
-        'type': 'uint16',
-    })
+        r = requests.get(url, headers={'x-api-key': key}, params={
+            'length': len(strategies),
+            'type': 'uint16',
+        })
 
-    if r.status_code == 200:
-        result = r.json()['data']
-        biggest = max(result)
-        if result.count(biggest) == 1:
-            return jsonify({'strategy': strategies[result.index(biggest)]})
+        if r.status_code == 200:
+            result = r.json()['data']
+            biggest = max(result)
+            if result.count(biggest) == 1:
+                return jsonify({'strategy': strategies[result.index(biggest)]})
+            else:
+                return jsonify({'strategy': 'Uncertain reading. Possibilities:\n' + '\n'.join(
+                    strategies[i] for i, x in enumerate(result) if x == biggest)})
         else:
-            return jsonify({'strategy': 'Uncertain reading. Possibilities:\n' + '\n'.join(
-                strategies[i] for i, x in enumerate(result) if x == biggest)})
+            return jsonify({'error': 'API request failed'}), r.status_code
     else:
-        return jsonify({'error': 'API request failed'}), r.status_code
+        # for testing, just return a random strategy
+        import random
+        return jsonify({'strategy': random.choice(strategies)[0]})
 
 
 @app.route('/')
 def index():
     # return index.html template
-    return render_template('index.html')
+    wait_time = 20
+
+    return render_template('index.html', wait_time=wait_time)
 
 
 if __name__ == '__main__':
--- strategies.py
+++ strategies.py
@@ -1,113 +1,153 @@
-strategies = """Abandon normal instruments
+strategies = """(Organic) machinery
+A line has two sides
+Abandon normal instruments
 Accept advice
 Accretion
-A line has two sides
 Allow an easement (an easement is the abandonment of a stricture)
+Animal noises
 Are there sections? Consider transitions
+Ask a computer program to repeat your last action
 Ask people to work against their better judgement
 Ask your body
 Assemble some of the instruments in a group and treat the group
+Back up a few steps. What else could you have done?
 Balance the consistency principle with the inconsistency principle
 Be dirty
+Be extravagant
+Be less critical more often
 Breathe more deeply
-Bridges -build -burn
+Build bridges
+Burn bridges
 Cascades
 Change instrument roles
 Change nothing and continue with immaculate consistency
-Children's voices -speaking -singing
+Children's voices singing
+Children's voices speaking
 Cluster analysis
 Consider different fading systems
-Consult other sources -promising -unpromising
+Consult other promising sources
+Consult other unpromising sources
 Convert a melodic element into a rhythmic element
 Courage!
+Credibility of intentions
 Cut a vital connection
 Decorate, decorate
-Define an area as `safe' and use it as an anchor
-Destroy -nothing -the most important thing
+Define an area as 'safe' and use it as an anchor
+Destroy nothing
+Destroy the most important thing
 Discard an axiom
+Disciplined self-indulgence
 Disconnect from desire
 Discover the recipes you are using and abandon them
+Display your talent
 Distorting time
 Do nothing for as long as possible
+Do something boring
+Do something sudden, destructive and unpredictable
+Do the washing up
+Do the words need changing?
+Do we need holes?
 Don't be afraid of things because they're easy to do
 Don't be frightened of cliches
 Don't be frightened to display your talents
 Don't break the silence
 Don't stress one thing more than another
-Do something boring
-Do the washing up
-Do the words need changing?
-Do we need holes?
+Earth
 Emphasize differences
 Emphasize repetitions
 Emphasize the flaws
-Faced with a choice, do both (given by Dieter Rot)
+Faced with a choice, do both!
 Feedback recordings into an acoustic situation
 Fill every beat with something
+Fire
 Get your neck massaged
 Ghost echoes
 Give the game away
 Give way to your worst impulse
+Go outside. Shut the door
 Go slowly all the way round the outside
+Heart
 Honor thy error as a hidden intention
+How would you explain this to your parents?
 How would you have done it?
 Humanize something free of error
+Humility of intentions
 Imagine the music as a moving chain or caterpillar
 Imagine the music as a set of disconnected events
 Infinitesimal gradations
-Intentions -credibility of -nobility of -humility of
+Instead of changing the thing, change the world around it
 Into the impossible
 Is it finished?
-Is there something missing?
 Is the tuning appropriate?
+Is there something missing?
+It is quite possible (after all)
+It is simply a matter or work
 Just carry on
 Left channel, right channel, centre channel
-Listen in total darkness, or in a very large room, very quietly
+Listen in a very large room, very quietly
+Listen in total darkness
 Listen to the quiet voice
 Look at a very small object, look at its centre
 Look at the order in which you do things
 Look closely at the most embarrassing details and amplify them
-Lowest common denominator check -single beat -single note -single riff
+Lost in useless territory
+Lowest common denominator check: single beat; single note; single riff
 Make a blank valuable by putting it in an exquisite frame
-Make an exhaustive list of everything you might do and do the last thing on the list
 Make a sudden, destructive unpredictable action; incorporate
+Make an exhaustive list of everything you might do and do the last thing on the list
+Make what's perfect more human
 Mechanicalize something idiosyncratic
+Move towards the impossible
+Move towards the unimportant
 Mute and continue
+Nobility of intentions
+Not building a wall but making a brick
+Once the search is in progress, something will be found
 Only one element of each kind
-(Organic) machinery
 Overtly resist change
+Pay attention to distractions
 Put in earplugs
+Question the heroic approach
 Remember those quiet evenings
 Remove ambiguities and convert to specifics
 Remove specifics and convert to ambiguities
+Remove the middle, extend the edges
 Repetition is a form of change
+Retrace your steps
 Reverse
 Short circuit (example: a man eating peas with the idea that they will improve his virility shovels them straight into his lap)
 Shut the door and listen from outside
 Simple subtraction
 Spectrum analysis
+State the problem in words as simply as possible
 Take a break
 Take away the elements in order of apparent non-importance
-Tape your mouth (given by Ritva Saarikko)
+Tape your mouth
 The inconsistency principle
+The most important thing is the thing most easily forgotten
 The tape is now the music
 Think of the radio
 Tidy up
 Trust in the you of now
+Try faking it
 Turn it upside down
 Twist the spine
+Use 'unqualified' people
 Use an old idea
 Use an unacceptable color
 Use fewer notes
 Use filters
-Use 'unqualified' people
+Voice your suspicions
 Water
-What are you really thinking about just now? Incorporate
 What is the reality of the situation?
 What mistakes did you make last time?
+What were you really thinking about just now? Incorporate
+What would make this really successful?
 What would your closest friend do?
-What wouldn't you do?
+What wouldn't you do? Do that
+Wind
 Work at a different speed
+Would anyone want it?
 You are an engineer
 You can only make one dot at a time
 You don't have to be ashamed of using your own ideas
